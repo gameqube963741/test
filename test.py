f78f8d39573a5d237963cec7997a5e8b3f6ed022
EdwarChiu <gameqube963741@gmail.com>
2020-08-22 21:13:33 +0800

    first commit

--- test.py
+++ test.py
@@ -1,12 +1,5 @@
-
-# import tkinter
-
-# window = tkinter.Tk()
-
-# window.mainloop()
-
 import tkinter as tk
 win = tk.Tk()
 win.geometry("450x100")
-win.title('123')
+win.title('main window')
 win.mainloop()
